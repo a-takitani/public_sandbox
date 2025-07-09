daa714e593e81b4d0f30fd50436d7dfc332db36c
Akihiro.Takitani <akihiro.takitani@money-design.com>
2025-07-09 15:54:55 +0900

    spring init

---
+++ src/test/java/app/sandbox/SandBoxApplicationTests.java
@@ -0,0 +1,11 @@
+package app.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandBoxApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
